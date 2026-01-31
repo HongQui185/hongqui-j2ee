907dfc117cb81b79bf01fe753b1d0fd5ad07d874
HongQui185 <Hongqui22003@gmail.com>
2026-01-31 11:20:37 +0700

    update

---
+++ demo/src/main/java/com/hqdemo/demo/controller/BookController.java
@@ -0,0 +1,54 @@
+package com.hqdemo.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hqdemo.demo.model.Book;
+import com.hqdemo.demo.service.BookService;
+
+//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+
+public class BookController {
+    @Autowired
+    private BookService bookService;
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable int id) {
+        return bookService.getBookById(id);
+    }
+
+    @PostMapping
+    public String addBook(@RequestBody Book book) {
+        bookService.addBook(book);
+        return "Book added successfully";
+    }
+
+    @PutMapping("/{id}")
+    public String updateBook(@PathVariable int id, @RequestBody Book updatedBook) {
+        bookService.updateBook(id, updatedBook);
+        return "Book updated successfully";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteBook(@PathVariable int id) {
+        bookService.deleteBook(id);
+        return "Book deleted successfully";
+    }
+
+    
+}
